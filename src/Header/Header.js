41804e4df830ea08a5ca6495ee60312bbcd05575
Allison Wagner <allisonjw01@hotmail.com>
2021-05-15 14:50:18 -0600

    Starting styling of homepage and header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import './Header.css';
+import './Header.scss';
+import logo from '../CardImages/poker-clip-art.png';
 
 export const Header = () => {
   return (
-    <div className="header_section">
-          Header to show something cool
-    </div>
+    <section className="header_section">
+          <img className="home_logo1" alt="card logo" src={logo}/>
+          <h2 className="header_h2">Welcome<span><br />Player</span></h2>
+          <img className="home_logo2" alt="card logo" src={logo}/>
+    </section>
   );
 }
 
